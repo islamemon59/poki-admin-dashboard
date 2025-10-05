1af697cb3068d0faa76676514502f7b523c039bc
islamemon59 <islamemon59@gmail.com>
2025-10-05 20:28:05 +0600

    router context provider setup complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { router } from "./Router/Router";
+import { RouterProvider } from "react-router";
+import AuthProvider from "./Context/AuthProvider";
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router}></RouterProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
